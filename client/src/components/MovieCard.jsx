61ea01c2727aab7c9961d76c38f4d66e6faea8be
nethra0906 <nethra.krish0906@gmail.com>
2025-12-17 01:38:46 +0530

    added more files

---
+++ client/src/components/MovieCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MovieCard = () => {
+  return (
+    <div className = 'flex flex-col justify-between p-3 bg-gray-800 rounded-2xl hover:-translate-y-1 transition duration-300 w-66'>
+       
+    </div>
+  )
+}
+
+export default MovieCard
