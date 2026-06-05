f0af6187d19ca210d730766e736cbeae2bd44afc
nethra0906 <nethra.krish0906@gmail.com>
2026-06-05 20:43:21 +0530

    added models and inngest

---
+++ server/inngest/index.js
@@ -0,0 +1,8 @@
+import { Inngest } from "inngest";
+
+export const inngest = new Inngest({ id: "movie-ticket-booking" });
+
+
+
+
+export const functions = [];
--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@ import express from "express";
 import cors from "cors";
 import 'dotenv/config';
 import connectDB from "./configs/db.js";
+import { clerkMiddleware } from '@clerk/express'
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js"
 
 const app = express();
 const PORT = 3000;
@@ -10,11 +13,14 @@ console.log("URI:", process.env.MONGODB_URI);
 await connectDB();
 
 app.use(cors());
+app.use(clerkMiddleware())
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello from the server!");
 });
+
+app.use('/api/inngest', serve({ client: inngest, functions }))
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
