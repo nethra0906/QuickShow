97c3e60ecb0e90f32b51b72761169f3bdc6f158e
nethra0906 <nethra.krish0906@gmail.com>
2026-06-10 23:57:53 +0530

    fixed error

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,60 +1,84 @@
 import { Inngest } from "inngest";
 import User from "../models/User.js";
 
-export const inngest = new Inngest({ id: "movie-ticket-booking" });
 
+export const inngest = new Inngest({
+  id: "movie-ticket-booking",
+});
+
+// 
 const syncUserCreation = inngest.createFunction(
-  {
-    id: "sync-user-from-clerk",
-    trigger: {
-      event: "clerk/user.created",
-    },
-  },
+  { id: "sync-user-from-clerk" },
+  { event: "clerk/user.created" },
   async ({ event }) => {
-    const { id, first_name, last_name, email_addresses, image_url } =
-      event.data;
+    try {
+      const { id, first_name, last_name, email_addresses, image_url } =
+        event.data;
 
-    await User.create({
-      _id: id,
-      email: email_addresses[0].email_address,
-      name: `${first_name} ${last_name}`,
-      image: image_url,
-    });
+      await User.create({
+        _id: id,
+        email: email_addresses?.[0]?.email_address,
+        name: `${first_name ?? ""} ${last_name ?? ""}`.trim(),
+        image: image_url,
+      });
+
+      console.log("User created:", id);
+    } catch (err) {
+      console.error("Error creating user:", err);
+      throw err;
+    }
   }
 );
 
+
 const syncUserDeletion = inngest.createFunction(
-  {
-    id: "delete-user-with-clerk",
-    trigger: {
-      event: "clerk/user.deleted",
-    },
-  },
+  { id: "delete-user-with-clerk" },
+  { event: "clerk/user.deleted" },
   async ({ event }) => {
-    const { id } = event.data;
-    await User.findByIdAndDelete(id);
+    try {
+      const { id } = event.data;
+
+      console.log("Deleting user:", id);
+
+      await User.findByIdAndDelete(id);
+
+      console.log("User deleted:", id);
+    } catch (err) {
+      console.error("Error deleting user:", err);
+      throw err;
+    }
   }
 );
 
 
 const syncUserUpdation = inngest.createFunction(
-  {
-    id: "update-user-from-clerk",
-    trigger: {
-      event: "clerk/user.updated",
-    },
-  },
+  { id: "update-user-from-clerk" },
+  { event: "clerk/user.updated" },
   async ({ event }) => {
-    const { id, first_name, last_name, email_addresses, image_url } =
-      event.data;
+    try {
+      const { id, first_name, last_name, email_addresses, image_url } =
+        event.data;
 
-    await User.findByIdAndUpdate(id, {
-      _id: id,
-      email: email_addresses[0].email_address,
-      name: `${first_name} ${last_name}`,
-      image: image_url,
-    });
+      await User.findByIdAndUpdate(
+        id,
+        {
+          email: email_addresses?.[0]?.email_address,
+          name: `${first_name ?? ""} ${last_name ?? ""}`.trim(),
+          image: image_url,
+        },
+        { new: true }
+      );
+
+      console.log("User updated:", id);
+    } catch (err) {
+      console.error("Error updating user:", err);
+      throw err;
+    }
   }
 );
 
-export const functions = [syncUserCreation, syncUserDeletion, syncUserUpdation];
+export const functions = [
+  syncUserCreation,
+  syncUserDeletion,
+  syncUserUpdation,
+];
