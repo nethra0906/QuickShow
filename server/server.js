b0bce1a055b9a2506d2c6c91f89778aa1071d3fb
nethra0906 <nethra.krish0906@gmail.com>
2026-06-14 19:50:30 +0530

    added routes

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import connectDB from "./configs/db.js";
 import { clerkMiddleware } from '@clerk/express'
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js"
+import showRouter from "./routes/showRoutes.js";
 
 const app = express();
 const PORT = 3000;
@@ -21,6 +22,8 @@ app.get("/", (req, res) => {
 });
 
 app.use('/api/inngest', serve({ client: inngest, functions }))
+
+app.use("/api/show", showRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
