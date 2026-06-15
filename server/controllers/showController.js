177f0f3ce71d0bd7f85ec360660eee275944a606
nethra0906 <nethra.krish0906@gmail.com>
2026-06-15 23:45:08 +0530

    added further api functions

--- server/controllers/showController.js
+++ server/controllers/showController.js
@@ -1,5 +1,8 @@
-
 import axios from 'axios';
+import Movie from "../models/Movie.js";
+import Show from "../models/Show.js";
+
+console.log("Movie =", Movie);
 
 export const getNowPlayingMovies = async (req, res) => {
     try {   
@@ -16,4 +19,64 @@ export const getNowPlayingMovies = async (req, res) => {
         res.json({ success: false, message: 'Error fetching now playing movies' });
     }
 }
+
+export const addShow = async (req, res) => {
+    try {
+        const { movieId, showsInput, showPrice } = req.body;  
+        let movie = await Movie.findById(movieId);
+
+        if (!movie) {
+            const [movieDetailsResponse, movieCreditsResponse] = await Promise.all([
+                axios.get(`https://api.themoviedb.org/3/movie/${movieId}`, 
+                    {
+                     headers: {Authorization: `Bearer ${process.env.TMDB_API_KEY}`}
+                }),
+
+
+                axios.get(`https://api.themoviedb.org/3/movie/${movieId}/credits`, {
+                    headers: { Authorization: `Bearer ${process.env.TMDB_API_KEY}` }
+                })
+            ]);
+
+            const movieApiData = movieDetailsResponse.data;
+            const movieCreditsData = movieCreditsResponse.data;
+
+            const movieDetails = {
+                _id: movieId,
+                title: movieApiData.title,
+                overview: movieApiData.overview,
+                poster_path: movieApiData.poster_path,
+                backdrop_path: movieApiData.backdrop_path,
+                genres: movieApiData.genres,
+                release_date: movieApiData.release_date,
+                casts: movieCreditsData.cast,
+                original_language: movieApiData.original_language,
+                tagline: movieApiData.tagline || '',
+                vote_average: movieApiData.vote_average,
+                runtime: movieApiData.runtime,
+                
+            };
+
+            movie = await Movie.create(movieDetails);
+        }
+
+        const showsToCreate = [];
+        showsInput.forEach(show => {
+            const showDate = show.date;
+            show.time.forEach((time) => {
+                const dateTimeString = `${showDate}T${time}`;
+                showsToCreate.push({ movie: movieId, showDateTime: new Date(dateTimeString), showPrice, occupiedSeats: {}});
+            });
+        });
+
+        if(showsToCreate.length > 0) {
+            await Show.insertMany(showsToCreate);
+        }
+        res.json({ success: true, message: 'Shows added successfully' });
+    }
+    catch (error) {
+        console.error(error);
+        res.json({ success: false, message: error.message });
+    }
+};
     
--- server/models/Show.js
+++ server/models/Show.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const ShowSchema = new mongoose.Schema({
     movie: { type: String, required: true , ref: 'Movie'},
     showDateTime: { type: Date, required: true },
-    ShowPrice: { type: Number, required: true },
+    showPrice: { type: Number, required: true },
     occupiedSeats: { type: Object, required: true },
 }, {minimize:false}
 );
