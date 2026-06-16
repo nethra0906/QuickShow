de381db79fa464b657634b541fd3de3a37ff7780
nethra0906 <nethra.krish0906@gmail.com>
2026-06-16 23:05:18 +0530

    added a few more get functions

--- server/controllers/showController.js
+++ server/controllers/showController.js
@@ -2,8 +2,6 @@ import axios from 'axios';
 import Movie from "../models/Movie.js";
 import Show from "../models/Show.js";
 
-console.log("Movie =", Movie);
-
 export const getNowPlayingMovies = async (req, res) => {
     try {   
         const {data} = await axios.get('https://api.themoviedb.org/3/movie/now_playing', {
@@ -80,3 +78,52 @@ export const addShow = async (req, res) => {
     }
 };
     
+export const getShows = async (req, res) => {
+  try {
+    const shows = await Show.find()
+  .populate("movie")
+  .sort({ showDateTime: 1 });
+    const uniqueShowsMap = new Map();
+
+    shows.forEach((show) => {
+      uniqueShowsMap.set(show.movie._id.toString(), show.movie);
+    });
+
+    res.json({
+      success: true,
+      shows: Array.from(uniqueShowsMap.values()),
+    });
+  } catch (error) {
+    console.error(error);
+    res.json({
+      success: false,
+      message: "Error fetching shows",
+    });
+  }
+};
+
+export const getShow = async (req, res) => {
+    try{
+        const {movieId} = req.params;
+        const shows = await Show.find({movie: movieId, showDateTime: {$gte: new Date()}})
+
+        const movie = await Movie.findById(movieId);
+        const dateTime = {};
+
+        shows.forEach((show) => 
+        {
+            const date = show.showDateTime.toISOString().split('T')[0];
+            if(!dateTime[date]) {
+                dateTime[date] = [];
+            }
+            dateTime[date].push({time: show.showDateTime, showId: show._id});
+        }) 
+        res.json({ success: true, movie, dateTime });
+    }
+
+    catch(error)
+    {
+        console.error(error);
+        res.json({ success: false, message: 'Error fetching show details' });
+    }
+}
--- server/routes/showRoutes.js
+++ server/routes/showRoutes.js
@@ -2,10 +2,14 @@ import express from "express";
 import { getNowPlayingMovies } from "../controllers/showController.js";
 import { addShow } from "../controllers/showController.js";
 import { protectAdmin } from "../middleware/auth.js";
+import { getShows, getShow } from "../controllers/showController.js";
 
 const showRouter = express.Router();
 
 showRouter.get("/now-playing", protectAdmin, getNowPlayingMovies);
 showRouter.post("/add", protectAdmin, addShow);
+showRouter.get("/all", getShows);
+showRouter.get("/:movieId", getShow);
+
 
 export default showRouter;
