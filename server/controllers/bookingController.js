ae13d8ed8032e3b866616639e04987f34e42d6ea
nethra0906 <nethra.krish0906@gmail.com>
2026-06-18 02:20:31 +0530

    completed Booking Controllers.js

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -24,3 +24,68 @@ const checkSeatsAvailability = async(showId, selectedSeats) =>
         return false;
     }
 }
+
+export const createBooking = async (req, res) =>
+{
+    try 
+    {
+        const {userId} = req.auth();
+        const {showId, selectedSeats} = req.body;
+        const {origin} = req.headers;
+
+        const isAvailable = await checkSeatsAvailability(showId, selectedSeats);
+
+        if(!isAvailable)
+        {
+            return res.json({success: false, error: "Selected seats are not available"})
+        }
+
+        const showData = await Show.findById(showId).populate("movie");
+        const booking = await Booking.create({
+            user: userId, 
+            show: showId, 
+            amount: showData.price * selectedSeats.length, 
+            bookedSeats: selectedSeats
+        });
+
+        selectedSeats.map((seat) => {
+            showData.occupiedSeats[seat] = userId;
+        });
+
+        showData.markModified("occupiedSeats");
+        await showData.save();
+
+        res.json({success: true, message: "Booking created successfully"})
+    }
+
+    catch(error)
+    {
+        console.log(error.message)
+        return res.status(500).json({success: false, error: "Internal server error"})
+    }
+}
+
+export const getOccupiedSeats = async (req, res) =>
+{
+    try
+    {
+        const {showId} = req.params;
+        const showData = await Show.findById(showId);
+
+        const occupiedSeats = Object.keys(showData.occupiedSeats);
+
+        if(!showData)
+        {
+            return res.json({success: false, error: "Show not found"})
+        }
+
+        res.json({success: true, occupiedSeats: occupiedSeats})
+    }
+
+    catch(error)
+    {
+        console.log(error.message);
+
+        return res.status(500).json({success: false, error: "Internal server error"})
+    }
+}
