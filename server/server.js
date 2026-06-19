11bc5d5c5e35ab4232459b00a632f7ed16c7a5f9
nethra0906 <nethra.krish0906@gmail.com>
2026-06-20 00:31:27 +0530

    added the admin functions and routes

--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ import { clerkMiddleware } from '@clerk/express'
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js"
 import showRouter from "./routes/showRoutes.js";
+import bookingRouter from "./routes/bookingRoutes.js";
+import adminRouter from "./routes/adminRoutes.js";
 
 const app = express();
 const PORT = 3000;
@@ -26,6 +28,8 @@ app.use('/api/inngest', serve({ client: inngest, functions }))
 app.use("/api/show", showRouter);
 
 app.use('/api/booking', bookingRouter);
+
+app.use('/api/admin', adminRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
