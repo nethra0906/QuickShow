b2097194a1b1ebdeaabad8435a81d22f38e684ac
nethra0906 <nethra.krish0906@gmail.com>
2026-06-21 01:41:10 +0530

    added userControllers and routes

---
+++ server/controllers/userController.js
@@ -0,0 +1,71 @@
+import Booking from "../models/Booking.js";
+import { clerkClient } from "@clerk/express";
+import Movie from "../models/Movie.js";
+
+export const getUserBookings = async(req, res) =>
+{
+    try {
+        const user = req.auth().userId;
+
+        const bookings = await Booking.find({user}).populate({
+            path: "show",
+            populate: {path: "movie"}
+        }).sort({createdAt: -1})
+
+        res.json({success: true, bookings})
+
+    } catch (error) {
+        console.error(error.message);
+        res.json({success:false, message:error.message});
+    }
+}
+
+export const updateFavorites = async(req, res) => {
+    try {
+
+        const{movieId} = req.body;
+        const userId = req.auth().userId;
+
+        const user = await clerkClient.users.getUser(userId);
+
+        if(!user.privateMetadata.favorites)
+        {
+            user.primaryMeta.favorites = []
+        }
+        
+        if(!user.privateMetadata.favorites.includes(movieId))
+        {
+            user.privateMetadata.favorites.push(movieId)
+        }
+
+        else
+        {
+             user.privateMetadata.favorites =  user.privateMetadata.favorites.filter(item => item !== movieId)
+        }
+
+        await clerkClient.users.updateUserMetadata(userId, {privateMetadata: user.privateMetadata})
+
+        res.json({success: true, message: "Favorite added updated."})
+
+        
+    } catch (error) {
+        console.error(error.message);
+        res.json({success:false, message:error.message});
+    }
+}
+
+export const getFavorites = async(req, res) =>
+{
+    try {
+        const user = await clerkClient.users.getUser(req.auth().userId)
+        const favorites = user.privateMetadata.favorites;
+
+        const movies = await Movie.find({_id: {$in: favorites}})
+
+        res.json({success: true, movies})
+        
+    } catch (error) {
+        console.error(error.message);
+        res.json({success:false, message:error.message});
+    }
+}
